675cdd92abf27bfda4595501c6efac446dcf8ec1
markoopapa <markoopapa@ymail.com>
2026-02-05 11:01:49 +0200

    Update customemailmanager.php

--- customemailmanager.php
+++ customemailmanager.php
@@ -50,30 +50,69 @@ private function installDefaultTemplates()
 
     public function hookActionEmailSendBefore($params)
 {
+    // 1. Alapadatok kinyerése
     $template_vars = &$params['template_vars'];
-    
-    // Csak akkor futunk le, ha van rendelés az adatok között
+    $active_id = (int)Configuration::get('CUSTOM_EMAIL_ACTIVE_ID');
+
+    // 2. Termékképes lista generálása (ha van rendelés)
     if (isset($template_vars['{id_order}'])) {
         $order = new Order((int)$template_vars['{id_order}']);
         $products = $order->getProducts();
-        $items_html = '';
+        $items_html = '<table width="100%" style="border-collapse: collapse;">';
 
         foreach ($products as $product) {
-            // Termékkép URL generálása
             $image = Image::getCover($product['product_id']);
-            $image_path = Context::getContext()->link->getImageLink($product['link_rewrite'], $image['id_image'], 'small_default');
+            $image_path = Context::getContext()->link->getImageLink(
+                $product['link_rewrite'], 
+                $image['id_image'], 
+                'small_default'
+            );
 
-            // Megépítjük a saját HTML sorunkat képpel
             $items_html .= '
-            <tr style="border-bottom:1px solid #eee;">
-                <td style="padding:10px;"><img src="'.$image_path.'" width="50"></td>
-                <td style="padding:10px;"><strong>'.$product['product_name'].'</strong><br><small>Ref: '.$product['product_reference'].'</small></td>
-                <td style="padding:10px; text-align:right;">'.Tools::displayPrice($product['total_price_tax_incl']).'</td>
+            <tr style="border-bottom:1px solid #edf2f7;">
+                <td style="padding:12px 8px; width:60px;">
+                    <img src="'.$image_path.'" width="50" style="border-radius:4px; display:block;">
+                </td>
+                <td style="padding:12px 8px;">
+                    <div style="font-weight:600; color:#1a1c21; font-size:14px;">'.$product['product_name'].'</div>
+                    <div style="color:#718096; font-size:12px;">Ref: '.$product['product_reference'].'</div>
+                </td>
+                <td style="padding:12px 8px; text-align:right; font-weight:600; color:#1a1c21;">
+                    '.Tools::displayPrice($product['total_price_tax_incl']).'
+                </td>
             </tr>';
         }
+        $items_html .= '</table>';
 
-        // Kicseréljük a gyári {items} változót a miénkre
+        // Lecseréljük a gyári {items} változót a mi profi listánkra
         $template_vars['{items}'] = $items_html;
+    }
+
+    // 3. Sablon csere (Wrapper logika)
+    // Ha van kiválasztott aktív sablon az adminban
+    if ($active_id > 0) {
+        $template_data = new CustomEmailTemplate($active_id);
+        
+        if (Validate::isLoadedObject($template_data)) {
+            $full_html = $template_data->content_html;
+            
+            // Kicseréljük az összes változót (pl. {shop_name}, {items}, {total_paid}) a sablonban
+            foreach ($template_vars as $key => $value) {
+                // Biztosítjuk, hogy csak szöveges adatokat cserélünk
+                if (is_string($value) || is_numeric($value)) {
+                    $full_html = str_replace($key, $value, $full_html);
+                }
+            }
+
+            // A PrestaShop e-mail küldőjének átadjuk a módosított tartalmat
+            // Ezzel felülírjuk a gyári alapértelmezett fájlt
+            $params['cart_display'] = $full_html; 
+            
+            // Néhány Mail modulnál a 'template_html' paramétert is érdemes állítani a biztonság kedvéért
+            if (isset($params['template_html'])) {
+                $params['template_html'] = $full_html;
+            }
+        }
     }
 }
 
